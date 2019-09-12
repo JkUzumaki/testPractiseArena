a92e27eb7fcb3bf2cea6c47510e6dd1a819e9a36
jaikumar <jaikumar.1415@yahoo.com>
2019-09-12 17:36:49 +0530

    message queue basic

---
+++ Assignment_doc_prepare/00_test/ipc/Message_queue/m02_rcv.cpp
@@ -0,0 +1,23 @@
+#include<sys/ipc.h>
+//#include<sys/types.h>
+#include<sys/msg.h>
+#include<stdio.h>
+
+struct msg_buff{
+	long mtype;
+	char mtext[20];
+}msg;
+
+int main()
+{
+	key_t key = ftok("progfile", 3);
+	int fd = msgget(key, 0777|IPC_CREAT);
+	msg.mtype=1;
+	
+	msgrcv(fd, &msg,sizeof(msg), 1, 0);
+	
+	printf("Message received to the queue: %s\n", msg.mtext);
+	msgctl(fd, IPC_RMID, NULL);
+	return 0;	
+}
+
