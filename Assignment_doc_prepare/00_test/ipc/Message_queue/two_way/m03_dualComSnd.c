bfb252343cd5f554b59fe951c9572853051653bc
jaikumar <jaikumar.1415@yahoo.com>
2019-09-13 12:08:59 +0530

    complete message queue

--- Assignment_doc_prepare/00_test/ipc/Message_queue/two_way/m03_dualComSnd.c
+++ Assignment_doc_prepare/00_test/ipc/Message_queue/two_way/m03_dualComSnd.c
@@ -13,7 +13,7 @@ int main()
 
 	key_t key = ftok("progfile", 3);
 	int fd = msgget(key, 0777|IPC_CREAT);
-	char arr[10] = "bye";
+
 	if(fork() == 0){
 		//SEND
 		//msgsnd (msgid, cosnt void *msgp, size_t msgz, int msgflg)
@@ -24,9 +24,6 @@ int main()
 			gets(msg.mtext);
 			msgsnd(fd, &msg,sizeof(msg), 0);
 			printf("Message added to the queue: %s\n", msg.mtext);
-			if(strcmp(arr, msg.mtext) == 0)
-				exit(0);//break;
-			
 			}
 		}else{
 		//RECEIVE
