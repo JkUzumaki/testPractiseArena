7cd677346729d10ce0cf27d0a135c275942b9c37
Jayakumar <jaikumar.1415@yahoo.com>
2019-11-07 11:38:02 +0530

    Bitwise formula

---
+++ Assignment_doc_prepare/00_test/BitWiseFORMULA.cpp
@@ -0,0 +1,12 @@
+//To set a bit @ specific position
+//	a = a | (1 << position)
+
+//To unset a bit @ specific position
+//	a = a & (~(1 << position))
+
+//To check is a bit @ any position is 1 or 0
+// i = a & (1 << position)
+// if(i > 0)
+//	bit is 1 @ position
+// else
+//	bit is 0 @ position
